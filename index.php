547c690bbd7a67749d903badff14cd55998487d5
IcyAuk <92365145+IcyAuk@users.noreply.github.com>
2024-11-28 22:53:16 +0100

    PostgreSQL DSN Commit Test 1

--- index.php
+++ index.php
@@ -4,13 +4,14 @@ echo("Hello World (dev)");
 
 // Database configuration
 $host = 'c3l5o0rb2a6o4l.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com'; // Database host
+$port = 5432; // Database port
 $dbname = 'dafh0vcjis1ed8'; // Database name
 $username = 'udmjdtgjjs0jnn'; // Database username
 $password = 'p3df65259f67c7819aed051ef7c3121a7502e37f5b24c62979d818ab5dcd42cb2'; // Database password
 $charset = 'utf8mb4'; // Character set
 
 // DSN (Data Source Name) for PDO
-$dsn = "pgsql:host=$host;dbname=$dbname;user=$username;password=$password";
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$username;password=$password";
 
 try {
     // Create a PDO instance (Database connection)
