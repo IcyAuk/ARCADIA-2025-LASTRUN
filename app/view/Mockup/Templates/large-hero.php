532bca364d71c823bb6627af83e6a3b2459079aa
IcyAuk <r.nvessah@gmail.com>
2024-12-10 12:31:28 +0100

    working on the mockups

---
+++ app/view/Mockup/Templates/large-hero.php
@@ -0,0 +1,13 @@
+<!-- CONTENT 1 START -->
+<div class="px-4 pt-5 my-5 text-center border-bottom">
+            <h1 class="display-4 fw-bold text-body-emphasis">Grand avec un grand G!</h1>
+            <div class="col-lg-6 mx-auto">
+                <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+            </div>
+            <div class="overflow-hidden" style="max-height: 50vh;">
+                <div class="container px-5">
+                    <img src="assets/images/stock_zoo.jpg" class="img-fluid border rounded-3 mb-4" alt="Example image" width="700" height="500" loading="lazy">
+                </div>
+            </div>
+        </div>
+<!-- CONTENT 1 END -->
