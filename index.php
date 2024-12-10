e91811bcdfa8fef0efdec57a33b559b613da2700
IcyAuk <r.nvessah@gmail.com>
2024-12-10 11:30:42 +0100

    Backend severed from the branch.

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
+
+//FRONTEND MOCKUP BRANCH
+//FRONT CONTROLLER IS DISABLED
+
 // FRONT CONTROLLER
 // LOAD COMPOSER AND DOTENV LIBRARY
+
+/*
 require __DIR__ . "/vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+*/
 
 // LOAD DATABASE
 require_once("core/Model.php");
