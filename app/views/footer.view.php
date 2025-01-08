ee7bf55073f1bf9db97c227a762157cce96777cd
IcyAuk <92365145+IcyAuk@users.noreply.github.com>
2025-01-08 13:37:40 +0100

    credentials done

--- app/views/footer.view.php
+++ app/views/footer.view.php
@@ -1,1 +1,111 @@
-<h5>footer</h5>
+<footer class="py-5">
+    
+    <div class="row">
+        <div class="col-6 col-md-2 mb-3">
+            <h5>Section</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+            </ul>
+        </div>
+
+        <div class="col-6 col-md-2 mb-3">
+            <h5>Section</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+            </ul>
+        </div>
+
+        <div class="col-6 col-md-2 mb-3">
+            <h5>Section</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+            </ul>
+        </div>
+
+        <div class="col-md-5 offset-md-1 mb-3">
+            <form>
+                <h5>Subscribe to our newsletter</h5>
+                <p>Monthly digest of what's new and exciting from us.</p>
+                <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                    <label for="newsletter1" class="visually-hidden">Email address</label>
+                    <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+                    <button class="btn btn-primary" type="button">Subscribe</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="table-responsive">
+        <table class="table table-sm">
+            <thead>
+                <tr>
+                    <th scope="col">Jour</th>
+                    <th scope="col">Ouverture</th>
+                    <th scope="col">Fermeture</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Lundi</th>
+                    <td>08:00</td>
+                    <td>18:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Mardi</th>
+                    <td>08:00</td>
+                    <td>18:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Mercredi</th>
+                    <td>08:00</td>
+                    <td>18:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Jeudi</th>
+                    <td>08:00</td>
+                    <td>18:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Vendredi</th>
+                    <td>08:00</td>
+                    <td>18:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Samedi</th>
+                    <td>09:00</td>
+                    <td>17:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Dimanche</th>
+                    <td>Fermé</td>
+                    <td>Fermé</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+    </div>
+    
+    <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+            <p>© 2024 Company, Inc. All rights reserved.</p>
+        
+    </div>
+</footer>
+
+</main>
+</body>
+
+</html>
--- app/views/login.view.php
+++ app/views/login.view.php
@@ -1,16 +1,23 @@
 <?php $this->view('header'); ?>
 
-<h1>Login View</h1>
-<p>Zoo website</p>
+<h1>Se connecter</h1>
 
-<form method="post">
-    <label for="email">email</label>
-    <input type="email" id="email" name="email" required>
-
-    <label for="password">password</label>
-    <input type="password" id="password" name="password" required>
-
-    <button type="submit">Submit</button>
+<form method="post" class="needs-validation" novalidate>
+    <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="email" name="email" required>
+        <div class="invalid-feedback">
+            Please enter a valid email address.
+        </div>
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" name="password" required>
+        <div class="invalid-feedback">
+            Please enter your password.
+        </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 
 <?php $this->view('footer'); ?>
